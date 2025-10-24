b89a79cf50c3220117da609777c38ebc4923fefc
Sanjay Ram <j.sanjayramjs@gmail.com>
2025-10-24 20:15:08 +0530

    first commit

--- app.py
+++ app.py
@@ -71,7 +71,7 @@ def main():
     except FileNotFoundError:
         pass
     
-    st.markdown("<h1 style='text-align: center;'>🐱 Cat vs Dog Classifier 🐶</h1>", unsafe_allow_html=True)
+    st.markdown("<h1 style='text-align: center;'>Cat vs Dog Classifier </h1>", unsafe_allow_html=True)
     st.markdown("<p style='text-align: center; font-size: 18px; margin-bottom: 30px;'>Upload an image or provide URL to classify if it's a cat or dog with 90%+ accuracy!</p>", unsafe_allow_html=True)
     
     classifier = load_model()
@@ -109,7 +109,7 @@ def main():
         st.markdown("<div style='height: 50px;'></div>", unsafe_allow_html=True)
     
     # Enhanced input method selection
-    st.markdown("<h3 style='text-align: center; margin-bottom: 20px;'>Choose Input Method<br style='font-size:10px;'><span style='color: #FFD700;font-size:10px'>Note: This model is Trainned by Tamil nadu Animals</span></br></h3>", unsafe_allow_html=True)
+    st.markdown("<h3 style='text-align: center; margin-bottom: 20px;'>Choose Input Method<br style='font-size:10px;'><span style='color: #FFD700;font-size:10px'>Note: This model is trained specifically to recognize animals found in Tamil Nadu.</span></br></h3>", unsafe_allow_html=True)
     
     # Center the radio buttons
     col1, col2, col3 = st.columns([1, 2, 1])
